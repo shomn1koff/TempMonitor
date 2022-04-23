388e540ce0c88db35fa75b405c944fc6e0e830fe
Alex <lexxminer@gmail.com>
2022-04-23 23:03:40 +0300

    Initial Commit

---
+++ TempMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempMonitor
+//
+//  Created by Алексей Шомников on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
